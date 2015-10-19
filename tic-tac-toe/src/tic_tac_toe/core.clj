7b4998b58a217a9b8e51005cb1d8c010126b7cc2
Rick Hall <rickhall2000@hotmail.com>
2015-10-19 03:54:15 -0400

    initial commit

---
+++ tic-tac-toe/src/tic_tac_toe/core.clj
@@ -0,0 +1,20 @@
+(ns tic-tac-toe.core
+  (:require [clojure.core.async :as async :refer [<! >!]]))
+
+
+(defonce channel (async/chan (async/sliding-buffer 1)))
+
+(def fill-chan
+  (async/go-loop []
+    (>! channel (rand-int 9))
+    (<! (async/timeout 5000))
+    (recur)))
+
+(defn handle-message
+  [msg]
+  (println msg))
+
+(def drain-chan
+  (async/go-loop []
+    (handle-message (<! channel))
+    (recur)))
