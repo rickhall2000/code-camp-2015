e52fef26f4d4383601f703d551acf085252173ae
Rick Hall <rickhall2000@hotmail.com>
2015-10-23 23:16:09 -0400

    added slides

--- tic-tac-toe/src/tic_tac_toe/core.clj
+++ tic-tac-toe/src/tic_tac_toe/core.clj
@@ -1,5 +1,6 @@
 (ns tic-tac-toe.core
-  (:require [clojure.core.async :as async :refer [<! >!]]))
+  (:require [clojure.core.async :as async :refer [<! >!]]
+            [clojure.string :as str]))
 
 
 (defonce channel (async/chan (async/sliding-buffer 1)))
@@ -10,11 +11,58 @@
     (<! (async/timeout 5000))
     (recur)))
 
+(defn full-board?
+  [board]
+  (every? (fn [x] (not (= x "."))) board))
+
+(defn row->string
+  [row]
+  (str/join (interpose "|" row)))
+
+
 (defn handle-message
   [msg]
-  (println msg))
+  #_(println msg))
 
 (def drain-chan
-  (async/go-loop []
-    (handle-message (<! channel))
-    (recur)))
+  (async/go-loop [acc 0]
+    (handle-message  (<! channel))
+    (recur (inc acc))))
+
+(defn new-game
+  []
+  {:board (vec (repeat 9 "."))
+   :player "X"})
+
+(defn new-player
+  [player]
+  (if (= player "X")
+    "O"
+    "X"))
+
+(defn make-move
+  [{:keys [board player] :as game} move]
+  (if (= "." (nth board move))
+    {:board (assoc board move player)
+     :player (new-player player)}
+    game))
+
+(defn print-board
+  [board]
+  (->> board
+       (partition 3)
+       (map row->string)
+       (interpose "------")
+       (interpose "\n")
+       (str/join)
+       (println)))
+
+(def winning-poitions
+  [[0 1 2] [3 4 5] [6 7 8]
+   [0 3 6] [1 4 7] [2 4 8]
+   [0 4 8] [2 4 6]])
+
+(defn find-winner
+  [board positions]
+  (let [items (map (fn [pos] (nth board pos)) positions)]
+    (if (apply = items) (first items) nil)))
