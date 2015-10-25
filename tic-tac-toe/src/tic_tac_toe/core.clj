c2026bb70ce7e1fee55a9f8711580d278207d6de
Rick Hall <rickhall2000@hotmail.com>
2015-10-25 03:49:56 -0400

    add code from demo

--- tic-tac-toe/src/tic_tac_toe/core.clj
+++ tic-tac-toe/src/tic_tac_toe/core.clj
@@ -7,15 +7,53 @@
 
 (defn handle-message
   [msg]
-  (println msg))
+  #_(println (* msg msg)))
 
 (def drain-chan
-  (async/go-loop []
+  (async/go-loop [a 0]
     (handle-message  (<! channel))
-    (recur)))
+    (recur (inc a))))
 
 (def fill-chan
   (async/go-loop []
     (>! channel (rand-int 9))
     (<! (async/timeout 5000))
     (recur)))
+
+(defn new-game
+  []
+  {:board (vec (repeat 9 "."))
+   :player "X"})
+
+(defn row->str
+  [row]
+  (str/join (interpose "|" row)))
+
+(defn print-board
+  [board]
+  (->> board
+       (partition 3)
+       (map row->str)
+       (interpose "-----")
+       (interpose "\n")
+       (str/join)
+       (println)))
+
+(defn next-player
+  [player]
+  (if (= "X" player)
+    "O"
+    "X"))
+
+(defn move
+  [{:keys [board player] :as game} move]
+  (if (= "." (nth board move))
+    {:board (assoc board move player)
+     :player (next-player player)}
+    game))
+
+(defn destruct
+  [ [a b c & d] ]
+  d)
+
+
